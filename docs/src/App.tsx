1f925a700c0a27b2004f79df303628ecaef4b607
Helloyunho <me@helloyunho.xyz>
2026-07-23 21:44:33 +0900

    feat: input done

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,34 +1,9 @@
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger
-} from '@/registry/ui/hover-card'
-import { Avatar } from '@/registry/ui/avatar'
+import { Input } from '@/registry/ui/input'
 
 function App () {
   return (
-    <div className='w-full h-screen flex flex-col gap-4 justify-center items-center'>
-      <HoverCard>
-        <HoverCardTrigger
-          href='https://example.com'
-          target='_blank'
-          rel='noreferrer'
-          className='link font-medium tracking-[-0.39px]'
-        >
-          @example
-        </HoverCardTrigger>
-        <HoverCardContent>
-          <div className='flex gap-3'>
-            <Avatar src='/levish_avatar.png' alt='example' />
-            <div className='flex flex-col gap-1'>
-              <p className='font-medium'>example</p>
-              <p className='text-xs leading-normal text-(--text)/56'>
-                Building lily — a Svelte component library.
-              </p>
-            </div>
-          </div>
-        </HoverCardContent>
-      </HoverCard>
+    <div className='w-full h-screen bg-white flex flex-col gap-4 justify-center items-center'>
+      <Input type='email' placeholder='you@example.com' className='max-w-xs' />
     </div>
   )
 }
